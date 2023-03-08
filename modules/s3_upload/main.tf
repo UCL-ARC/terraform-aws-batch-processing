8057a9be87a8b828dee28a8a743337feb8228391
Ben Thomas <ben.thomas1984@gmail.com>
2023-03-08 14:56:47 +0000

    fix: s3 bucket name clash

--- modules/s3_upload/main.tf
+++ modules/s3_upload/main.tf
@@ -7,24 +7,6 @@ resource "random_id" "id" {
   byte_length = 5
 }
 
-resource "aws_s3_bucket" "s3_upload_bucket" {
-  bucket = local.s3_bucket_name
-}
-
-resource "aws_s3_bucket_intelligent_tiering_configuration" "tiering_bucket" {
-  bucket = aws_s3_bucket.s3_upload_bucket.id
-  name   = "EntireBucket"
-
-  tiering {
-    access_tier = "DEEP_ARCHIVE_ACCESS"
-    days        = 180
-  }
-  tiering {
-    access_tier = "ARCHIVE_ACCESS"
-    days        = 125
-  }
-}
-
 module "s3_upload_bucket" {
   source  = "terraform-aws-modules/s3-bucket/aws"
   version = "3.6.1"
@@ -42,3 +24,18 @@ module "s3_upload_bucket" {
   restrict_public_buckets = true
 
 }
+
+resource "aws_s3_bucket_intelligent_tiering_configuration" "tiering_bucket" {
+  bucket = module.s3_upload_bucket.s3_bucket_id
+  name   = "EntireBucket"
+
+  tiering {
+    access_tier = "DEEP_ARCHIVE_ACCESS"
+    days        = 180
+  }
+  tiering {
+    access_tier = "ARCHIVE_ACCESS"
+    days        = 125
+  }
+}
+
