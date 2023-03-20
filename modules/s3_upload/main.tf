afcfed957731581f401628cbc138af759e6ebbb8
Dan Giles <d.giles@ucl.ac.uk>
2023-03-20 11:53:36 +0000

    feat: Add lambda trigger to s3_upload (#7)
    Co-authored-by: Dan Giles <dangiles@Dans-MacBook-Pro.local>

--- modules/s3_upload/main.tf
+++ modules/s3_upload/main.tf
@@ -39,3 +39,77 @@ resource "aws_s3_bucket_intelligent_tiering_configuration" "tiering_bucket" {
   }
 }
 
+
+resource "aws_lambda_function" "lambda_s3_handler" {
+  function_name    = "process-s3-new-objects"
+  filename         = data.archive_file.lambda_zip_file.output_path
+  source_code_hash = data.archive_file.lambda_zip_file.output_base64sha256
+  handler          = "index.handler"
+  role             = aws_iam_role.iam_for_lambda.arn
+  runtime          = "nodejs14.x"
+}
+
+data "archive_file" "lambda_zip_file" {
+  type        = "zip"
+  source_file = "${path.module}/src/index.js"
+  output_path = "${path.module}/lambda.zip"
+}
+
+resource "aws_iam_role" "iam_for_lambda" {
+  name = "iam_for_lambda"
+
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": "lambda.amazonaws.com"
+      },
+      "Effect": "Allow",
+      "Sid": ""
+    }
+  ]
+}
+EOF
+  inline_policy {
+    name   = "lambda_logs_policy"
+    policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+        "Sid": "AllowLambdaFunctionToCreateLogs",
+        "Action": [ 
+            "logs:*" 
+        ],
+        "Effect": "Allow",
+        "Resource": [ 
+            "arn:aws:logs:*:*:*" 
+        ]
+    }
+  ]
+}
+EOF
+  }
+}
+
+resource "aws_lambda_permission" "allow_bucket_invoke_lambda" {
+  statement_id  = "AllowExecutionFromS3Bucket"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.lambda_s3_handler.arn
+  principal     = "s3.amazonaws.com"
+  source_arn    = module.s3_upload_bucket.s3_bucket_arn
+}
+
+resource "aws_s3_bucket_notification" "bucket_notification" {
+  bucket = module.s3_upload_bucket.s3_bucket_id
+
+  lambda_function {
+    lambda_function_arn = aws_lambda_function.lambda_s3_handler.arn
+    events              = ["s3:ObjectCreated:*"]
+  }
+
+  depends_on = [aws_lambda_permission.allow_bucket_invoke_lambda]
+}
