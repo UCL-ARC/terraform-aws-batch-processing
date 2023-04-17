ad4e447c5548441a34091ca5f672ef10654b7682
Dan Giles <d.giles@ucl.ac.uk>
2023-04-17 17:38:57 +0100

    fix: Updating step-functions and its policy to submit Batch (#42)
    Co-authored-by: Dan Giles <dangiles@Dans-MacBook-Pro.local>

--- modules/step_function/main.tf
+++ modules/step_function/main.tf
@@ -1,25 +1,31 @@
 locals {
-  definition_template = <<EOF
-{
-  "Comment": "Example Task",
-  "StartAt": "BATCH_JOB",
-  "States": {
-    "BATCH_JOB": {
-      "Type": "Task",
-      "End" : true,
-      "Resource" : "${var.batch_task_arn}"
-    }
-  }
+  account_id = data.aws_caller_identity.current.account_id
 }
-EOF
-}
+
+data "aws_caller_identity" "current" {}
 
 module "step_function" {
   source = "terraform-aws-modules/step-functions/aws"
 
   name       = "my-step-function"
-  definition = local.definition_template
-
+  definition = <<EOF
+  {
+    "Comment": "Example Task",
+    "StartAt": "BATCH_JOB",
+    "States": {
+      "BATCH_JOB": {
+        "Type": "Task",
+        "End": true,
+        "Resource": "arn:aws:states:${var.region}:${local.account_id}:batch:submitJob.sync",
+        "Parameters": {
+          "JobDefinition": "test-processing",
+          "JobName": "ProcessingBatchJob",
+          "JobQueue": "HighPriority"
+        }
+      }
+    }
+  }
+  EOF
 
   service_integrations = {
     batch_Sync = {
@@ -29,4 +35,42 @@ module "step_function" {
 
   type = "STANDARD"
 
+}
+
+
+data "aws_iam_policy_document" "assume_role" {
+  statement {
+    effect = "Allow"
+
+    principals {
+      type        = "Service"
+      identifiers = ["states.${var.region}.amazonaws.com"]
+    }
+
+    actions = ["sts:AssumeRole"]
+  }
+}
+
+resource "aws_iam_role" "role_for_sfn" {
+  name               = "sfn-role"
+  assume_role_policy = data.aws_iam_policy_document.assume_role.json
+}
+
+resource "aws_iam_policy" "sfn_batch_policy" {
+  name = "sfn_batch_execution"
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Action   = ["batch:SubmitJob"]
+        Effect   = "Allow"
+        Resource = "*"
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "batch-attach" {
+  role       = aws_iam_role.role_for_sfn.name
+  policy_arn = aws_iam_policy.sfn_batch_policy.arn
 }
--- modules/step_function/variables.tf
+++ modules/step_function/variables.tf
@@ -2,3 +2,8 @@ variable "batch_task_arn" {
   type        = string
   description = "Batch Task arn"
 }
+variable "region" {
+  type        = string
+  default     = "eu-west-2"
+  description = "The region to deploy into."
+}
