696d7f1b7575bf310a286b552c106601b8dedf70
Dan Giles <d.giles@ucl.ac.uk>
2023-07-21 12:16:34 +0100

    fix: Adding the queues (#57)
    Co-authored-by: Dan Giles <dangiles@Dans-MacBook-Pro.local>

--- modules/step_function/variables.tf
+++ modules/step_function/variables.tf
@@ -7,10 +7,10 @@ variable "region" {
   default     = "eu-west-2"
   description = "The region to deploy into."
 }
-# variable "batch_job_queues" {
-#   type        = map(any)
-#   description = "Batch Job Queue"
-#   default = {
-#     "job_queues" = "HighPriority"
-#   }
-# }
+variable "batch_job_queues" {
+  type        = map(any)
+  description = "Batch Job Queue"
+  default = {
+    "job_queues" = "HighPriority"
+  }
+}
