3f759edb1c8baeaffd51c26e03d56c6f62700c9d
Dan Giles <d.giles@ucl.ac.uk>
2023-07-21 08:41:04 +0100

    fix: Add execution_role to batch (#53)
    Co-authored-by: Dan Giles <dangiles@Dans-MacBook-Pro.local>

--- modules/step_function/main.tf
+++ modules/step_function/main.tf
@@ -9,7 +9,7 @@ data "aws_caller_identity" "current" {}
 module "step_function" {
   source = "terraform-aws-modules/step-functions/aws"
 
-  name       = "sfn-batch"
+  name       = "sfn-batch-test"
   definition = <<EOF
   {
     "Comment": "Example State Machine",
@@ -43,7 +43,7 @@ module "step_function" {
 
 
 resource "aws_iam_role" "role_for_sfn" {
-  name = "sfn-batch"
+  name = "sfn-batch-roke"
 
   assume_role_policy = jsonencode({
     Version = "2012-10-17"
@@ -61,7 +61,7 @@ resource "aws_iam_role" "role_for_sfn" {
 }
 
 resource "aws_iam_policy" "sfn_batch_policy" {
-  name = "sfn-batch"
+  name = "sfn-batch-policy"
   # Terraform's "jsonencode" function converts a
   # Terraform expression result to valid JSON syntax.
   policy = jsonencode({
