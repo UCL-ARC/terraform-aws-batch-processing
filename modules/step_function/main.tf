d1665d3ceeaeea6e6ddecff3db479580af295455
Dan Giles <d.giles@ucl.ac.uk>
2023-08-10 15:49:18 +0100

    fix: Step functions with correct flow through the DataSync tasks (#69)
    Co-authored-by: Dan Giles <dangiles@arcdesk10.cs.ucl.ac.uk>

--- modules/step_function/main.tf
+++ modules/step_function/main.tf
@@ -17,11 +17,47 @@ module "step_function" {
   "States": {
     "Data_Sync_s3_efs": {
       "Type": "Task",
-      "Next": "BATCH_JOB",
+      "Next": "Poll_DataSyncS3toEFS",
       "Parameters": {
         "TaskArn": "${var.datasync_task_s3_efs}"
       },
       "Resource": "arn:aws:states:::aws-sdk:datasync:startTaskExecution"
+    },
+    "Poll_DataSyncS3toEFS": {
+      "Type": "Task",
+      "Next": "Check_DataSyncS3toEFS",
+      "Parameters": {
+        "TaskExecutionArn.$": "$.TaskExecutionArn"
+      },
+      "Resource": "arn:aws:states:::aws-sdk:datasync:describeTaskExecution"
+    },
+    "Check_DataSyncS3toEFS": {
+      "Type": "Choice",
+      "Choices": [
+        {
+          "Not": {
+            "Variable": "$.Status",
+            "StringEquals": "SUCCESS"
+          },
+          "Next": "Wait"
+        },
+        {
+          "Variable": "$.Status",
+          "StringEquals": "SUCCESS",
+          "Next": "BATCH_JOB"
+        },
+        {
+          "Variable": "$.Status",
+          "StringEquals": "ERROR",
+          "Next": "HandleError"
+        }
+      ],
+      "Default": "HandleError"
+    },
+    "Wait": {
+      "Type": "Wait",
+      "Seconds": 300,
+      "Next": "Poll_DataSyncS3toEFS"
     },
     "BATCH_JOB": {
       "Type": "Task",
@@ -36,11 +72,63 @@ module "step_function" {
     },
     "Data_Sync_efs_s3": {
       "Type": "Task",
-      "End": true,
+      "Next": "Poll_DataSyncEFStoS3", 
       "Parameters": {
         "TaskArn": "${var.datasync_task_efs_s3}"
       },
       "Resource": "arn:aws:states:::aws-sdk:datasync:startTaskExecution"
+    },
+   "Poll_DataSyncEFStoS3": {
+      "Type": "Task",
+      "Next": "Check_DataSyncEFStoS3",
+      "Parameters": {
+        "TaskExecutionArn.$": "$.TaskExecutionArn"
+      },
+      "Resource": "arn:aws:states:::aws-sdk:datasync:describeTaskExecution"
+    },
+    "Check_DataSyncEFStoS3": {
+      "Type": "Choice",
+      "Choices": [
+        {
+          "Not": {
+            "Variable": "$.Status",
+            "StringEquals": "SUCCESS"
+          },
+          "Next": "WaitEFStoS3"
+        },
+        {
+          "Variable": "$.Status",
+          "StringEquals": "SUCCESS",
+          "Next": "EndState"
+        },
+        {
+          "Variable": "$.Status",
+          "StringEquals": "ERROR",
+          "Next": "HandleError"
+        }
+      ],
+      "Default": "HandleError"
+    },
+    "WaitEFStoS3": {
+      "Type": "Wait",
+      "Seconds": 300,
+      "Next": "Poll_DataSyncEFStoS3"
+    },
+    "HandleError": {
+      "Type": "Task",
+      "Resource": "arn:aws:states:::sns:publish",
+      "End": true,
+      "Parameters": {
+        "Message": {
+          "Message": "An error occurred during DataSync."
+        },
+        "TopicArn.$": "$.DataSync-Topic"
+      }
+    },
+    "EndState": {
+    "Type": "Pass",
+    "Result": "The DataSync process has completed successfully.",
+    "End": true
     }
   }
 }
@@ -57,7 +145,6 @@ module "step_function" {
   type = "STANDARD"
 
 }
-
 
 resource "aws_iam_policy" "sfn_batch_policy" {
   name = "sfn-batch-policy"
