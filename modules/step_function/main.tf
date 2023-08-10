e890def9339af7c3f27f0dc5a7df71b2884dcde0
Dan Giles <dangiles@arcdesk10.cs.ucl.ac.uk>
2023-08-10 15:18:37 +0100

    fix: Step functions with correct flow through the DataSync tasks

--- modules/step_function/main.tf
+++ modules/step_function/main.tf
@@ -9,42 +9,13 @@ data "aws_caller_identity" "current" {}
 module "step_function" {
   source = "terraform-aws-modules/step-functions/aws"
 
-  name       = "sfn-datasync-batch"
-  definition = <<EOF
-  {
-  "Comment": "Example State Machine",
-  "StartAt": "Data_Sync_s3_efs",
-  "States": {
-    "Data_Sync_s3_efs": {
-      "Type": "Task",
-      "Next": "BATCH_JOB",
-      "Parameters": {
-        "TaskArn": "${var.datasync_task_s3_efs}"
-      },
-      "Resource": "arn:aws:states:::aws-sdk:datasync:startTaskExecution"
-    },
-    "BATCH_JOB": {
-      "Type": "Task",
-      "Resource": "arn:aws:states:::batch:submitJob.sync",
-      "Parameters": {
-        "JobDefinition": "${var.batch_task_arn}",
-        "JobQueue": "${local.job_queue.arn}",
-        "JobName": "simple",
-        "ShareIdentifier": "test"
-      },
-      "Next": "Data_Sync_efs_s3"
-    },
-    "Data_Sync_efs_s3": {
-      "Type": "Task",
-      "End": true,
-      "Parameters": {
-        "TaskArn": "${var.datasync_task_efs_s3}"
-      },
-      "Resource": "arn:aws:states:::aws-sdk:datasync:startTaskExecution"
-    }
-  }
-}
-  EOF
+  name = "sfn-datasync-batch"
+  definition = templatefile("${path.module}/sfn_definition.json.tmpl",
+    { datasyncS3ToEFS = var.datasync_task_s3_efs,
+      BatchTaskArn    = var.batch_task_arn,
+      JobQueue        = local.job_queue.arn,
+  datasyncEFSToS3 = var.datasync_task_efs_s3 })
+
   service_integrations = {
     batch_Sync = {
       events = true
